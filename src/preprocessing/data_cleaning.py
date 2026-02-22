2a22c75cd9e5bed87192cee10c28b344c85131c1
Phillip Smith <pesmithiii7@gmail.com>
2026-02-22 14:51:21 -0600

    applied KNN for data imputation

--- src/preprocessing/data_cleaning.py
+++ src/preprocessing/data_cleaning.py
@@ -22,17 +22,4 @@ def clean_raw_data(df):
         'Serious Delinquencies in past 2 years': 'serious_delinquencies_past_2_years'
     })
 
-    is_missing_value(df, col_orig='monthly_revenue', col_bool='revenue_missing')
-    is_missing_value(df, col_orig='debt_ratio', col_bool='debt_ratio_missing')
-    is_missing_value(df, col_orig='rated_exposure', col_bool='rated_exposure_missing')
-
-    return df
-
-def is_missing_value(df, col_orig: str = None, col_bool: str = None):
-    df[col_bool] = df[col_orig].isna().astype(int)
-    return df
-
-def fill_missing_value(df, col: str = None):
-    col_mean = df[col].mean()
-    df[col] = df[col].fillna(col_mean)
     return df
